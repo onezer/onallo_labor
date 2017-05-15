15097370021384b8c97cb11144e9c4cc1ce949dc
pastiv <pastivv@gmail.com>
2017-05-15 19:34:50 +0200

    ...

---
+++ Assets/_CompletedAssets/Scripts/Managers/WavePowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class WavePowerUp : MonoBehaviour
+    {
+
+        public float DamageModifier;
+        public float HpModifier;
+        public float XPModifier;
+
+
+    }
+}
