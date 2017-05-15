db9bd9554944e8aa2122d76cf8124217266473ca
pastiv <pastivv@gmail.com>
2017-05-15 23:26:00 +0200

    R Ability (BombRain) Added Bug: Area signaling circle isn't visible

---
+++ Assets/_CompletedAssets/Scripts/Player/Abilities/BombRain.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+
+    public class BombRain : MonoBehaviour
+    {
+        public int BombNumber;
+        public float TimeBetweenBombs;
+        public float Radius;
+        public int Damage;
+        public float ExplosionRange;
+
+        public GameObject Bomb;
+
+        float timer = 0;
+
+        void Awake()
+        {
+            transform.localScale = new Vector3(Radius,Radius,1);
+        }
+
+        Vector3 RandomPosition()
+        {
+            float rx;
+            float rz;
+
+            rx = Random.Range(-Radius, Radius);
+            rz = Random.Range(-Mathf.Sqrt(Radius * Radius - rx * rx), Mathf.Sqrt(Radius * Radius - rx * rx));
+
+            return new Vector3(rx + transform.position.x, 8, rz + transform.position.z);
+        }
+        
+        void Update()
+        {
+
+            if (timer >= TimeBetweenBombs && BombNumber > 0)
+            {
+                Bomb = Instantiate(Bomb);
+                Bomb.transform.position = RandomPosition();
+                Bomb.GetComponent<BombMechanics>().range = ExplosionRange;
+                Bomb.GetComponent<BombMechanics>().damage = Damage;
+
+                BombNumber--;
+                timer = 0;
+            }
+            timer += Time.deltaTime;
+
+            if(BombNumber == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/_CompletedAssets/Scripts/Player/Abilities/BombRainAbility.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class BombRainAbility : Ability
+    {
+
+        BombRain Mechanics;
+        GameObject CurrentRain;
+
+        public int BombNumber;
+        public float TimeBetweenBombs;
+        public float Radius;
+        public int Damage;
+        public float ExplosionRange;
+
+        public override void Activate()
+        {
+            CurrentRain = Instantiate(AbilityGameObj);
+            CurrentRain.transform.position = new Vector3(CursorPoint.x, 1f, CursorPoint.z);
+            CurrentRain.transform.rotation = new Quaternion(0.7f, 0.3f, -0.3f, 0.7f);
+            Debug.Log(CurrentRain.transform.rotation);
+            Mechanics = CurrentRain.GetComponent<BombRain>();
+            Mechanics.BombNumber = BombNumber;
+            Mechanics.TimeBetweenBombs = TimeBetweenBombs;
+            Mechanics.Radius = Radius;
+            Mechanics.Damage = Damage;
+            Mechanics.ExplosionRange = ExplosionRange;
+        }
+
+    }
+}
